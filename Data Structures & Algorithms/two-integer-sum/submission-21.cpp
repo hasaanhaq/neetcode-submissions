4a3086530589bbbc7d3095a351fc7bc9520737f7
Hasaan Haq <69989533+hasaanhaq@users.noreply.github.com>
2026-09-15 14:00:02 -0700

    Add: two-integer-sum - submission-21

---
+++ Data Structures & Algorithms/two-integer-sum/submission-21.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> numMap;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(numMap.count(target - nums[i])){
+                return {numMap[target - nums[i]], i};
+            }else{
+                numMap[nums[i]] = i;
+            }
+        }
+
+        return {};
+    }
+};
